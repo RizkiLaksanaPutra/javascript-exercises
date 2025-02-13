2c5852b7f59e8db0f8515a4c54b395c6e3167dda
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-02-13 15:45:58 +0700

    Completing lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return arr = string.split('').reverse().join('')
 };
 
+console.log(reverseString('Hello World!'))
 // Do not edit below this line
 module.exports = reverseString;
