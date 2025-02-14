9783dc2cf45d51523b982a8c51a986414312c76c
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-02-14 15:32:37 +0700

    Completing lesson 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const century = year % 100 === 0;
+  const leap4 = year % 4 === 0;
+  const leap400 = year % 400 === 0;
 
+  if (leap4 && (!century || leap400)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
