a96fc428140e1587080946888dd436e455214372
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-02-14 11:14:07 +0700

    Completing lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) return 'ERROR';
+  if (firstNumber < 0 || secondNumber < 0) return 'ERROR';
+  if (firstNumber > secondNumber) [firstNumber, secondNumber] = [secondNumber, firstNumber]
+  let result = 0;
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
