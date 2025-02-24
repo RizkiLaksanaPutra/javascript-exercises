b6fad3044f6183e9ca24df4b1870025a77dad33c
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-02-24 21:26:05 +0700

    Completing lesson 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let cleanedString = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let reverseStr = cleanedString.split("").reverse().join("");
+  return reverseStr === cleanedString;
 };
 
 // Do not edit below this line
