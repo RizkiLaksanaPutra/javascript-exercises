feb0a6c1576c22d94baa254276dc50118a354065
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-02-24 20:42:37 +0700

    Completing lesson 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([...args]) {
+  let result = 0;
+  for (let i of [...args]) {
+    result += i;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function([...args]) {
+  let result = 1;
+  for (let i of [...args]) {
+    result *= i;
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
