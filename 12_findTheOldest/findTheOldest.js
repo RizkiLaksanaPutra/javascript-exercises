c7e28137d37389560664248a271342fe27d3c3b0
Rizki Laksana Putra <120088587+RizkiLaksanaPutra@users.noreply.github.com>
2025-03-01 00:06:30 +0700

    Completing lesson 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people
+    .map((person) => {
+      if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
+      return {
+        name: person.name,
+        age: person.yearOfDeath - person.yearOfBirth,
+      };
+    })
+    .sort((a, b) => b.age - a.age)
+    .reduce((oldest, person) => {
+      return oldest.age > person.age ? oldest : person;
+    });
 };
 
 // Do not edit below this line
